8974a1413864dc61dfb053ff66d5f0b797b090da
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2026-01-13 19:19:31 +0530

    Add files via upload

---
+++ Day 2 of dsa/first.cpp
@@ -0,0 +1,92 @@
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// cout << "Hello CoderArmy" ;
+// return 0 ;
+// };
+
+
+
+// // datatypes 
+
+// //print int
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// // integer
+// int num = 500 ;
+// cout << num;
+// return }
+
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// // Character
+// char c = 'a' ;
+// cout << c;
+// return 0 ;
+// };
+
+
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// // double
+// double num = 93.3265 ;
+// cout << num;
+// return}
+
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// // float
+// float d = 1.32 ;
+// cout << d;
+// return 0 ;
+// };
+
+
+// Typecasting: A type cast is basically a conversion from one type to another.
+// int main ()
+// {
+// int num = 'A' ;
+// cout << (num);
+// return 0 ;
+// };
+// int main ()
+// {
+// char num = 65 ;
+// cout << (num);
+// return 0 ;
+// };
+// Output : 65 Output : A
+// Here, At first the compiler converts the code into binary numbers. Then, Binary numbers convert into output
+
+
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// int num = 0 ;
+// if (num > 0 )
+// {
+// cout << "It is Positive Number" ;
+// }
+// else if (num < 0 )
+// {
+// cout << "It is Negative Number" ;
+// }
+// else
+// {
+// cout << "It is zero Number" ;
+// }
+// return
