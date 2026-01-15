df970f040d3fbc7ae91d819f4c3d4ba759b9d3f3
Ankuj matre <154369381+ankuj07@users.noreply.github.com>
2026-01-15 19:25:02 +0530

    Add files via upload

---
+++ Day 4 of dsa/first.cpp
@@ -0,0 +1,102 @@
+// // lets started for loop
+
+// // print any no. table
+// // #include <iostream>
+// // using namespace std;
+
+// // int main()
+// // {
+// //     cout << "Enter your no ";
+// //     int n;
+// //     cin >> n;
+
+// //     for (int i = 1; i <= 10; i++)
+// //     {
+// //         cout << n << "*" << i << "=" << n * i << endl;
+// //     }
+// //     return 0;
+// // }
+
+// // kisi bhi no. ka fibonaki nikalo
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int num;
+//     cin >> num;
+
+//     if (num == 1)
+//     {
+//         cout << '1';
+//         return 0;
+//     }
+//     if (num == 2)
+//     {
+//         cout << '1';
+//         return 0;
+//     }
+
+//     int first_number = 0;
+//     int second_number = 1;
+//     int current_number;
+
+//     for (int i = 3; i <= num; i++)
+//     {
+
+//         // main logic ye hai
+
+//         current_number = first_number + second_number;
+//         first_number = second_number;
+//         second_number = current_number;
+//     }
+
+//     cout << current_number;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 3 * 2 - 10 / 5;
+//     cout << n;
+//     return 0;
+// };
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 3 * 2 - 10 / 5;
+//     cout << n;
+//     return 0;
+// };
+
+
+// // pattern
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// int n = 3 * 2 - 10 / 5 ;
+// cout << n;
+// return 0 ;
+// };
+
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+// int n = 3 * 2 - 10 / 5 ;
+// cout << n;
+// return 0 ;
+// };
